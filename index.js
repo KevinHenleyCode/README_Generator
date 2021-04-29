0d3981148c0006b1c7aabf16d4ba88094cfc768c
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-04-29 05:05:18 -0500

    able to add license badges

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const fs = require('fs');
 
 
 
+
 const readmeContent = (userInput) =>
+
     `
 # ${userInput.title}
+${userInput.license}
 <br>
 
 ## Description
@@ -13,6 +16,12 @@ const readmeContent = (userInput) =>
 <br>
 
 ## Table of Contents
+- [Installation](#Installation) <br>
+- [Usage](#Usage) <br>
+- [License](#License) <br>
+- [Contributing](#Contributing) <br>
+- [Tests](#Tests) <br>
+- [Questions](#Questions) <br>
 <br>
 
 ## Installation
@@ -24,9 +33,10 @@ const readmeContent = (userInput) =>
 <br>
 
 ## License
+${licenseInfo}
 <br>
 
-## Contribution
+## Contributing
     ${userInput.contribution}
 <br>
 
@@ -35,6 +45,10 @@ const readmeContent = (userInput) =>
 <br>
 
 ## Questions
+GitHub: https://github.com/${userInput.github}
+
+For more questions you can reach me at my Email:(${userInput.email})
+
 <br>
 
     `;
@@ -62,20 +76,78 @@ inquirer.prompt([
         message: 'What is this programs intended perpose?'
     },
     {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license.',
+        choices: [
+            {
+                key: 'AGPL v3', 
+                value: '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
+            }, 
+            {
+                key:'GPL v3',
+                value: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+            },
+            {
+                key: 'LGPL v3',
+                value: "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)"
+            }, 
+            {
+                key: 'MPL 2.0',
+                value: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+            }, 
+            {
+                key: 'Apache 2.0',
+                value: "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+            }, 
+            {
+                key: 'MIT',
+                value: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+            }, 
+            {
+                key: 'Boost 1.0',
+                value: "[![License: Boost 1.0](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+            }, 
+            {
+                key: 'Unlicense',
+                value: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+            }],
+           
+    
+    },
+    {
         type: 'input',
-        name: 'contribution',
+        name: 'contributing',
         message: 'How can people contribute to this project?'
     },
     {
         type: 'input',
-        name: 'test',
+        name: 'tests',
         message: 'How can you test this program?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub user name?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
     }
 ])
 .then((userInput) => {
 
     const readmeDoc = readmeContent(userInput);
 
+    if (userInput.license == '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)') {
+        let licenseInfo = `A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an 
+        express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+    }
+
+
     fs.writeFile('README.md', readmeDoc, (error) =>
     error ? console.log(error) : console.log('Your README file is ready!'));
 });
+
+
